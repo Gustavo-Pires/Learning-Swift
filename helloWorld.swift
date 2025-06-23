87b89d784462db48337b8f12376526ad184c8690
Gustavo Pires Bertaco <85207790+Gustavo-Pires@users.noreply.github.com>
2025-06-22 22:42:35 -0300

    Create helloWorld.swift

---
+++ helloWorld.swift
@@ -0,0 +1,10 @@
+// Este é um comentário em Swift, igual ao Python com #
+
+// Declara uma constante chamada 'mensagem' e atribui um valor de string a ela
+let mensagem = "Hello World!"
+
+// Imprime a mensagem no console
+print(mensagem)
+
+// Imprimindo uma string diretamente:
+print("Bem-vindo ao mundo Swift!")
