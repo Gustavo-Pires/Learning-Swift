7751df66d1fe6e898e7867da2e0e2f5baf914a85
Gustavo Pires Bertaco <85207790+Gustavo-Pires@users.noreply.github.com>
2025-06-23 22:19:35 -0300

    Create variaveis.swift

---
+++ variaveis.swift
@@ -0,0 +1,5 @@
+var idade = 30 // Declara uma variável que pode ser alterada
+idade = 31     // Altera o valor de idade
+
+let nome = "Maria" // Declara uma constante, não pode ser alterada
+// nome = "João"    // Isso geraria um erro!
